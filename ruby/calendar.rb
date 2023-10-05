3d3592fb733a2affadd55c2e5c6e4fcaeec6e0ee
futaro0405 <132374957+futaro0405@users.noreply.github.com>
2023-10-05 12:09:22 +0900

    Update calendar.rb

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -5,13 +5,13 @@ class SetParams
   def initialize
     @options = {}
     OptionParser.new do |opt|
-      opt.on('-y', '--year VAL', 'description:specify the display year') { |val| @options[:year] = val}
-      opt.on('-m', '--month VAL', 'description:specify the display month') { |val| @options[:month] = val}
+      opt.on('-y', '--year VAL', 'description:specify the display year') { |val| @options[:year] = val }
+      opt.on('-m', '--month VAL', 'description:specify the display month') { |val| @options[:month] = val }
       opt.parse!(ARGV)
     end
   end
 
-  # オプションが指定されたか判定する
+  # オプションが指定されたか
   def has?(option)
     @options.include(option)
   end
@@ -22,29 +22,53 @@ class SetParams
   end
 end
 
+class Calendar
+  attr_reader :target_year, :target_month
 
-option = SetParams.new
-set_year = (option.get(:year) || Date.today.year).to_i
-set_month = (option.get(:month) || Date.today.month).to_i
+  def initialize(target_year, target_month)
+    @target_year = target_year
+    @target_month = target_month
+  end
 
-target_date = Date.new(set_year, set_month, 1)
-year = target_date.year
-month = target_date.month
-head = target_date.strftime("%m月 %Y")
-week_display = %w(日 月 火 水 木 金 土)
+  def judge(year, month)
+    if year.negative? || year > 9999
+      raise "#{year} is an invalid number (0..9999)"
+    elsif month.negative? || month > 12
+      raise "#{month} is neither a month number (1..12) nor a name"
+    end
+  end
 
-first_day = Date.new(year, month, 1)
-last_day = Date.new(year, month, -1)
-first_wday = first_day.wday
+  def output
+    judge(@target_year, @target_month)
 
-puts head.center(20)
-puts week_display.join(" ")
-print "   " * first_wday
-(1..last_day.day).each do |day|
-  print day.to_s.rjust(2)
-  if (first_wday + day)%7 == 0
-    puts "\n"
-  else
-    print " "
+    head = "#{@target_month}月 #{@target_year}"
+    week_display = %w[月 火 水 木 金 土 日]
+    first_day = Date.new(@target_year, @target_month, 1)
+    last_day = Date.new(@target_year, @target_month, -1)
+    first_wday = if first_day.sunday?
+                   6
+                 else
+                   first_day.wday - 1
+                 end
+
+    puts head.center(20)
+    puts week_display.join(' ')
+    print '   ' * first_wday
+    (1..last_day.day).each do |day|
+      print day.to_s.rjust(2)
+      if ((first_wday + day) % 7).zero?
+        puts "\n"
+      else
+        print ' '
+      end
+    end
   end
 end
+
+option = SetParams.new
+calendar = Calendar.new(
+  (option.get(:year) || Time.zone.today.year).to_i,
+  (option.get(:month) || Time.zone.today.month).to_i
+)
+
+calendar.output
