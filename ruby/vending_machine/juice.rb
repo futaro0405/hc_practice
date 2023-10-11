a5c10a96d293c07e7ed43c013bf9d6ccb91e354f
futaro0405 <132374957+futaro0405@users.noreply.github.com>
2023-10-11 17:24:33 +0900

    Create juice.rb

---
+++ ruby/vending_machine/juice.rb
@@ -0,0 +1,20 @@
+class Juice
+
+  def initialize(name, money, number = 1)
+    @name = name
+    @money = money
+    @number = number
+  end
+
+  def name
+    @name
+  end
+
+  def money
+    @money
+  end
+
+  def number
+    @number
+  end
+end
