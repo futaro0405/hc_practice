308283bf65abfd2826eefcffdce7ec660fc7a5a4
futaro0405 <132374957+futaro0405@users.noreply.github.com>
2023-10-16 15:17:44 +0900

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,41 +1,12 @@
 const { parseArgs } = require("node:util");
 
-const options = {
-  year: {
-    type: "string",
-    short: "y",
-    multiple: false,
-  },
-  month: {
-    type: "string",
-    short: "m",
-    multiple: false,
-  },
-};
+"use strict";
 
-const args = process.argv.slice(2);
-const parsedArgs = parseArgs({ options, args });
-
-const today = new Date();
-const setYear = parsedArgs.values.year || today.getFullYear();
-const setMonth = parsedArgs.values.month - 1 || today.getMonth();
-
-try {
-  if (setYear < 0 || setYear > 9999) {
-    throw new Error(`${setYear} is an invalid number (0..9999)`);
-  }else if(setMonth < 1 || setMonth > 12) {
-    throw new Error(`${setMonth} is neither a month number (1..12) nor a name`);
-  }
-} catch(e) {
-  console.error(e.message);
-}
-
-const week = ["日", "月", "火", "水", "木", "金", "土"];
-let showDate = new Date(setYear, setMonth, 1);
-
-function showProcess(date) {
+const show = (date) => {
   let year = date.getFullYear();
   let month = date.getMonth();
+  const week = ["日", "月", "火", "水", "木", "金", "土"];
+
   console.log(`      ${month + 1}月 ${year}`);
   console.log(week.join(" "));
 
@@ -58,5 +29,39 @@ function showProcess(date) {
   console.log(arrDay.join(""));
 }
 
+const setOption = () => {
+  const options = {
+    year: {
+      type: "string",
+      short: "y",
+      multiple: false,
+    },
+    month: {
+      type: "string",
+      short: "m",
+      multiple: false,
+    },
+  };
 
-showProcess(showDate);
+  const args = process.argv.slice(2);
+  const parsedArgs = parseArgs({options, args});
+  const today = new Date();
+  const setYear = parsedArgs.values.year || today.getFullYear();
+  const setMonth = parsedArgs.values.month -1 || today.getMonth();
+
+  if(setMonth < 0 || setMonth > 11) {
+    throw new Error(`${setMonth + 1} is neither a month number (1..12) nor a name`);
+  }else if (setYear < 1000 || setYear > 9999) {
+    throw new Error(`${setYear} is an invalid number (1000..9999)`);
+  }
+
+  return [setYear, setMonth];
+};
+
+try {
+  let [year, month] = setOption();
+  let showDate = new Date(year, month, 1);
+  show(showDate);
+} catch(e) {
+  console.error(e.message);
+}
