4ee2d908130a8e87957362ffa3be019143ba29f5
futaro0405 <132374957+futaro0405@users.noreply.github.com>
2023-11-06 17:36:08 +0900

    Update main.js

--- src/common/js/main.js
+++ src/common/js/main.js
@@ -1,8 +1,33 @@
 (() => {
-  const outputData = (jsonAll) => {
+  const apiKey ={
+    all: "https://ihatov08.github.io/kimetsu_api/api/all.json",
+    hashira: "https://ihatov08.github.io/kimetsu_api/api/hashira.json",
+    oni: "https://ihatov08.github.io/kimetsu_api/api/oni.json",
+    kisatsu: "https://ihatov08.github.io/kimetsu_api/api/kisatsutai.json"
+  }
+
+  const filter = document.getElementById('js-filter');
+  const categories = filter.querySelectorAll('input[name=category]');
+
+  const selectItem = () => {
+    for(let i = 0; i < categories.length; i++) {
+      if(categories.item(i).checked) {
+        checkValue = categories.item(i).value;
+        break;
+      }
+    }
+
+    callApi(apiKey.checkValue);
+  }
+
+  categories.forEach((category) => {
+    category.addEventListener('change', selectItem);
+  });
+
+  const outputData = (json) => {
     const template = document.querySelector('.js-panel__template').content;
     const fragment = document.createDocumentFragment();
-    for(const data of jsonAll) {
+    for(const data of json) {
       const clone = document.importNode(template, true);
       const cloneName = clone.querySelector('.m-panel__name');
       const cloneBadge = clone.querySelector('.m-panel__badgeItem');
@@ -19,14 +44,9 @@
     document.getElementById('js-panel').appendChild(fragment);
   };
 
-  const callApi = async () => {
-    // const resAll = await fetch("https://ihatov08.github.io/kimetsu_api/api/all.json");
-    // const resHashira = await fetch("https://ihatov08.github.io/kimetsu_api/api/hashira.json ");
-    // const resOni = await fetch("https://ihatov08.github.io/kimetsu_api/api/oni.json");
-    // const resKisatsu = await fetch("https://ihatov08.github.io/kimetsu_api/api/kisatsutai.json");
-
+  const callApi = async (apiKey) => {
     try {
-      const res = await fetch("https://ihatov08.github.io/kimetsu_api/api/all.json");
+      const res = await fetch(apiKey);
       if(!res.ok) {
         throw new Error("API Can't be loaded")
       }
@@ -38,11 +58,9 @@
     }
   };
 
-  
+  // const init = () => {
+  //   callApi();
+  // };
 
-  const init = () => {
-    callApi();
-  };
-
-  init();
+  // init();
 })();
